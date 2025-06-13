95d77d8109d9a4488edd524eaa22ecec3be52f5b
cherkasovaa <alina.ch1202@gmail.com>
2025-06-13 08:53:12 +0300

    chore: update vite config
    Add `vite-plugin-static-copy` for copying assets.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,31 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig(() => {
   return {
     root: 'src',
-    base: '/',
     build: {
-      outDir: './build',
+      outDir: '../build',
       emptyOutDir: true,
       assetsDir: 'assets',
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'src/index.html'),
+          gifts: resolve(__dirname, 'src/gifts.html'),
+        },
+      },
     },
+    plugins: [
+      viteStaticCopy({
+        targets: [
+          {
+            src: 'assets/**/*',
+            dest: 'assets',
+          },
+        ],
+      }),
+    ],
     server: {
       port: 3000,
       open: true,
